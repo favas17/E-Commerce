1ed17203c1fd513eede7cbd2da9f7c73fb0219f6
favas17 <favasfvs111@gmail,com>
2024-04-08 15:33:59 +0530

    product Model

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+const userSchema= new mongoose.Schema({
+    username:{
+        type:String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required: true,
+    },
+    confirmPassword:{
+        type:String,
+        required: false,
+    },
+    mobile:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type: Boolean,
+        default: false,
+    },
+    isBlocked:{
+        type: Boolean,
+        default: false,
+    }
+});
+
+
+const user = mongoose.model('User', userSchema);
+
+module.exports = user;
